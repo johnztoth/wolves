c2d865558c5b1968df439aab10ee8e5afc941bb5
johnztoth <gy20jt@leeds.ac.uk>
2021-04-18 15:20:26 +0530

    updated plotting

--- wolf.py
+++ wolf.py
@@ -10,23 +10,27 @@ As they eat the grass their store increases.
 The wolves randonly move by +/- 2 units at a time.
 If a wolf gets within eating_range of a sheep it eats the sheep.
 The wolf gets the sheep's store and the sheep agent is removed.
-
 """
 
 import matplotlib.pyplot
 import random
 import sheepclass
 import wolfclass
+import numpy as np
+
 
 num_of_sheep = 10
-num_of_wolves = 6
+num_of_wolves = 2
 num_of_iterations = 1000
 eating_range = 10
 grass_length = 100
 
-""" sheep list and wolf list """
+""" sheep list and wolf list, and the store totals for graphing"""
 sheep = []
 wolves = []
+shuffled = []
+sheep_store = np.zeros(num_of_iterations, dtype=int)
+wolf_store = np.zeros((num_of_iterations,num_of_wolves), dtype=int)
 
 """ create flat grass """
 grass=[]
@@ -61,31 +65,50 @@ for step in range(num_of_iterations):
         sheep[i].move()
         sheep[i].graze()
     
+    """ update the total sheep store """
+    for i in range(num_of_sheep):
+        sheep_store[step] += sheep[i].store
+
     """ move the wolves """
     for i in range(num_of_wolves):
         wolves[i].move()
 
     """ wolves eat the sheep if the sheep get too close """
     """ shuffle wolves so each wolf gets first chance to eat sheep """
-    random.shuffle(wolves)
+    shuffled = random.sample(wolves,len(wolves)) 
     for i in range(num_of_wolves):
-        sheep_temp=sheep[:]
+        sheep_temp=sheep[:]    # temporary list for calculating distances
         eaten=0 
         for j in range(num_of_sheep):
-            distance=distance_between(wolves[i],sheep_temp[j])
+            distance=distance_between(shuffled[i],sheep_temp[j])
             if distance <= eating_range:
-                del sheep[j-eaten]
-                print("sheep eaten by wolf")
+                del sheep[j-eaten]   # remove sheep from real list
+                print("iteration",step,"sheep eaten by wolf")
                 eaten += 1
-                wolves[i].store += sheep_temp[j].store
+                shuffled[i].store += sheep_temp[j].store
         num_of_sheep -= eaten
     
+    """ update the wolf store for each wolf """
+    for i in range(num_of_wolves):
+        wolf_store[step,i]=wolves[i].store
+
     """ break from iterations if no sheep left """
     if num_of_sheep == 0:
         print("zero sheep left")
         break
 
+
+""" plot the sheep store """
+matplotlib.pyplot.figure(1)
+matplotlib.pyplot.plot(sheep_store, color='blue')
+for i in range(num_of_wolves):
+    matplotlib.pyplot.plot(wolf_store[:,i])
+matplotlib.pyplot.xlabel("no of iterations")
+matplotlib.pyplot.ylabel("units stored")
+matplotlib.pyplot.show()
+
 """ plot the sheep, wolves and grass """
+matplotlib.pyplot.figure(2)
 matplotlib.pyplot.xlim(0, no_cols)
 matplotlib.pyplot.ylim(0, no_rows)
 matplotlib.pyplot.imshow(grass)
